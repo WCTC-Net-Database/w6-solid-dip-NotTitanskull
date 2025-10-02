3bd1c2f77792763637f4d14cf23659797706fc62
Christian Fenger <fengercs@gmail.com>
2025-10-02 14:59:16 -0500

    Added missing dependencies to the `ICharacter` interface

--- Interfaces/ICharacter.cs
+++ Interfaces/ICharacter.cs
@@ -4,7 +4,11 @@
     {
         void Attack(ICharacter target);
         void Move();
+        void UniqueBehavior();
         string Name { get; set; }
+        string Type { get; set; }
+        int Level { get; set; }
+        int HP { get; set; }
     }
 
 }
